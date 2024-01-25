2c9f241b26e284d27c928879e83a616add3842a3
Russell Gordon <rgordon@lcs.on.ca>
2024-01-24 19:10:38 -0500

    Initial Commit

---
+++ CustomFontsExample/CustomFontsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontsExampleApp.swift
+//  CustomFontsExample
+//
+//  Created by Russell Gordon on 2024-01-24.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
